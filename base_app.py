8ac8961e8592f70494a33921d4a1f0d761c678d3
NeyoCreator <neos25722@gmail.com>
2021-06-21 13:01:19 +0200

    added svm classifier

--- base_app.py
+++ base_app.py
@@ -46,21 +46,10 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Logistic Regression", "OnevsRest classifier"]
+	options = ["Logistic Regression", "SVM classifier"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
-	# Building out the "Information" page
 	if selection == "Logistic Regression":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
-
-	# Building out the predication page
-	if selection == "OnevsRest classifier":
 		#st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("","Enter recent tweet")
@@ -96,7 +85,48 @@ def main():
 				
 			else :
 				#st.image('img.jpg', width=60)
-				st.subheader("Available electric cars")
+				st.title("Available electric cars")
+				st.image('p1.png', width=500)
+				st.image('p2.png', width=500)
+
+	# Building out the predication page
+	if selection == "SVM classifier":
+		#st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("","Enter recent tweet")
+
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			#vect_text = tweet_cv.transform([tweet_text]).toarray()
+			tweet_text = [tweet_text]
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/ovr.pkl"),"rb"))
+			prediction = predictor.predict(tweet_text)
+			#st.image('hybrid1.2.png', width=500)
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
+			#st.success("Text Categorized as: {}".format(prediction))
+
+			st.title(prediction[0])
+
+			if (prediction[0] == -1) :
+				st.title("Gas cars avaialable")
+				st.image('a1.png', width=500)
+				st.image('a2.png', width=500)
+				st.image('a3.png', width=500)
+				
+				
+			elif (prediction[0]==0) :
+				st.title("Hybrid cars avaialable")
+				st.image('h1.png', width=500)
+				st.image('h2.png', width=500)
+				st.image('h3.png', width=500)
+				
+			else :
+				#st.image('img.jpg', width=60)
+				st.title("Available electric cars")
 				st.image('p1.png', width=500)
 				st.image('p2.png', width=500)
 				
